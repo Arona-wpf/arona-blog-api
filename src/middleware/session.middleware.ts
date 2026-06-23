587d610f12b783b335025878200a3c32ac52b047
Arona-wpf <290564581@qq.com>
2026-06-23 19:53:50 +0800

    PERF 更新session中间件的处理

--- src/middleware/session.middleware.ts
+++ src/middleware/session.middleware.ts
@@ -19,7 +19,7 @@ export class SessionMiddleware implements IMiddleware<Context, NextFunction> {
     return async (ctx: Context, next: NextFunction) => {
       const resultHelper = await ctx.requestContext.getAsync(ResultHelper);
       try {
-        // 公共api/cdn 不进行session校验
+        // 公共api/minio 不进行session校验
         if (
           ctx.path.startsWith('/public-api') ||
           ctx.path.startsWith('/minio')
@@ -27,9 +27,19 @@ export class SessionMiddleware implements IMiddleware<Context, NextFunction> {
           await next();
           return;
         }
-
+        // 获取 session 对象
         const session = ctx.session as unknown as IUserSession;
         if (!session?.user?.account) {
+          if (!session?.guest?.tmpId) {
+            this.logger.warn(
+              'Session exists but no user or guest info found, initializing guest session. current path: %s',
+              ctx.path
+            );
+            // 如果 session 中没有 user 和 guest 对象，初始化 guest 对象
+            session.guest = {
+              tmpId: randomId(),
+            };
+          }
           ctx.status = 401;
           ctx.body = {
             code: 401,
@@ -38,15 +48,16 @@ export class SessionMiddleware implements IMiddleware<Context, NextFunction> {
           };
           return;
         }
-
+        // 如果 session 中有 user 对象，继续处理请求
         await next();
-      } finally {
-        if (ctx.session && !ctx.session?.user && !ctx.session?.guest) {
-          // 如果 session 中没有 user 和 guest 对象，初始化 guest 对象
-          ctx.session.guest = {
-            tmpId: randomId(),
-          };
-        }
+      } catch (error) {
+        this.logger.error('SessionMiddleware error: ', error);
+        ctx.status = 500;
+        ctx.body = {
+          code: 500,
+          msg: resultHelper.translate('internal.server.error'),
+          success: false,
+        };
       }
     };
   }
